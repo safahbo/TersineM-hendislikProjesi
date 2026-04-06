76aba608617fa535b2201e5bac0a0fca4cffe8ff
safahbo <safahbo61@gmail.com>
2026-04-06 23:50:55 +0300

    Update security_audit.test.js

--- tests/security_audit.test.js
+++ tests/security_audit.test.js
@@ -11,3 +11,7 @@ if (Date.now() - start < 10) console.log("[OK] Regex motoru hızlı ve güvenli.
 console.log("[TEST] Entropi Hassasiyeti Kontrolü...");
 const highEntropy = calculateEntropy("aB1!sK9#mL2$nP0%"); 
 if (highEntropy > 4.0) console.log("[OK] Entropi motoru yüksek rastgeleliği yakalıyor.");
+const { calculateEntropy } = require('../src/utils');
+console.log("[TEST] Entropi Algoritması Doğrulanıyor...");
+const score = calculateEntropy("4c0nS7r4ct0r!"); 
+if (score > 3) console.log("[OK] Entropi motoru hassas veri tespiti için hazır.");
